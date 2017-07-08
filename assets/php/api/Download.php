007fa030fd954c667b2e1839a7d4b1eb8e547beb
l-x-0x7 <Victor Lourme@DESKTOP-K94C0CG>
2017-07-08 14:41:17 +0200

    ✔️ Fully working downloads

---
+++ assets/php/api/Download.php
@@ -0,0 +1,127 @@
+<?php
+
+/***********
+
+* ZeroSide *
+
+Open Source & Anonymous File Sharing
+
+************/
+
+namespace com\zeroside;
+
+class Download {
+
+    public function show($id){
+        global $db;
+	    $request = $db->prepare('SELECT * FROM files WHERE file_url=:id');
+    
+	    $request->execute(array(
+	    	":id" => $id
+	    ));
+
+	    $result = $request->fetch();
+
+	    if(empty($result)){
+	    	header('Location: /?r=404');
+	    } else {
+	    	# Testing expiration
+	    	if(time() > $result['file_time']){
+	    		# Setup Pug
+	    		$pug = new \Pug\Pug();
+
+	    		# Get file
+	    		$file = file_get_contents(__DIR__ . '/../../../views/download/expired.pug');
+
+	    		# Sending page
+	    		echo $pug->render($file, array(
+	    			"file_name" => $result[0]
+	    		));
+
+	    		# Remove download from database to prevent access it again
+	    		$request = $db->prepare('DELETE FROM files WHERE file_url=:id');
+	    		$request->execute(array(
+	    			":id" => $id
+	    		));
+
+	    		# Remove in folder now
+	    		unlink(__DIR__ . '/../../uploads/' . $result['file_path']);
+	    	} else {
+	    		# Setup Pug
+	    		$pug = new \Pug\Pug();	
+
+	    		# Get file
+	    		$file = file_get_contents(__DIR__ . '/../../../views/download/model.pug');
+
+	    		# Sending page
+	    		echo $pug->render($file, array(
+	    			"file_name" => $result[0],
+	    			"file_size" => $result[4],
+	    			"temp_url"  => base64_encode($id)
+	    		));
+
+	    		$request = $db->prepare('UPDATE files SET views=:views WHERE file_url=:id');
+	    		$request->execute(array(
+	    			":views" => $result['views'] + 1,
+	    			":id" => $id
+	    		));
+	    	}
+	    }
+    }
+
+    public function fire($id){
+
+        if(empty($id)){
+            header('Location: /?r=500');
+        } else {
+
+            global $db;
+            
+            $request = $db->prepare('SELECT * FROM files WHERE file_url=:id');
+            $request->execute(array(
+                ':id' => base64_decode($id)
+            ));
+
+            $result = $request->fetch();
+
+            $filename = __DIR__ . '/../../uploads/' . $result['file_path'];
+            $name = $result['file_name'];
+
+            $request = $db->prepare('UPDATE files SET stat_dl=:dl WHERE file_url=:id');
+            $request->execute(array(
+                ':dl' => $result['stat_dl'] + 1,
+                ':id' => base64_decode($id)
+            ));
+
+            // Starting file sending
+            set_time_limit(0);
+            session_start();
+ 
+            if (!is_file($filename) || !is_readable($filename)) {
+                header("HTTP/1.1 404 Not Found");
+                exit;
+            }
+            $size = filesize($filename);
+            
+            if (ini_get("zlib.output_compression")) {
+                ini_set("zlib.output_compression", "Off");
+            }
+
+            session_write_close();
+ 
+            header("Cache-Control: no-cache, must-revalidate");
+            header("Cache-Control: post-check=0,pre-check=0");
+            header("Cache-Control: max-age=0");
+            header("Pragma: no-cache");
+            header("Expires: 0");
+
+            header("Content-Type: application/force-download");
+            header('Content-Disposition: attachment; filename="'.$name.'"');
+
+            header("Content-Length: ".$size);
+
+            readfile($filename);
+        }
+    }
+
+}
--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define('GB', 1073741824);
 define('TB', 1099511627776);
 
 # Set locale UTF-8 US
-setlocale(LC_ALL,'en_US.UTF-8');
+setlocale(LC_ALL, 'en_US.UTF-8');
 
 # Init DB (MySQL)
 #-----------------
@@ -31,28 +31,38 @@ setlocale(LC_ALL,'en_US.UTF-8');
 # stat_id   | INT  -> Identifier to access stats page
 #-----------------
 
-$host = "localhost";
+$host     = "localhost";
 $database = "zeroside";
 $username = "root";
 $password = "";
 
-$db = new PDO(
-	'mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', 
-	$username, $password
-);
+$db = new \PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $username, $password);
 
 # Require composer modules
 require(__DIR__ . '/vendor/autoload.php');
 
 # Require API modules
 require(__DIR__ . '/assets/php/api/URL.php');
+require(__DIR__ . '/assets/php/api/Upload.php');
+require(__DIR__ . '/assets/php/api/Download.php');
 
 # Setup AltoRouter
 $router = new AltoRouter();
 
 # Readable human size
-function human_filesize($bytes, $decimals = 2) {
-    $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+function human_filesize($bytes, $decimals = 2)
+{
+    $size   = array(
+        'B',
+        'kB',
+        'MB',
+        'GB',
+        'TB',
+        'PB',
+        'EB',
+        'ZB',
+        'YB'
+    );
     $factor = floor((strlen($bytes) - 1) / 3);
     return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
 }
@@ -60,136 +70,68 @@ function human_filesize($bytes, $decimals = 2) {
 # Global user variables
 $id = uniqid();
 
-# Mapping homepage
-$router->map('GET', '/', function(){
-	# Setup Pug
-	$pug = new Pug\Pug();	
-
-	# Get file
-	$file = file_get_contents(__DIR__ . '/views/homepage.pug');
-
-	# Sending file
-	global $id;
-
-	echo $pug->render($file, array(
-		"id" => $id
-	));
+# [GET] Mapping homepage
+$router->map('GET', '/', function()
+{
+    # Setup Pug
+    $pug = new Pug\Pug();
+    
+    # Get file
+    $file = file_get_contents(__DIR__ . '/views/homepage.pug');
+    
+    # Sending file
+    global $id;
+    
+    echo $pug->render($file, array(
+        "id" => $id
+    ));
 });
 
-# Mapping download
-$router->map('GET', '/[i:id]', function( $id ) {
-	echo "download: {$id}";
+# [GET] Mapping download
+$router->map('GET', '/[a:id]', function($id)
+{
+    $download = new com\zeroside\Download;
+    $download->show($id);
+});
+
+# [GET] Mapping analytics
+$router->map('GET', '/s/[a:id]', function($id)
+{
+    
+});
+
+# [API] Mapping download base64
+$router->map('POST', '/api/download', function()
+{
+    $download = new com\zeroside\Download;
+    $download->fire($_POST['download_uid']);
 });
 
 # [API] Mapping url checker
-$router->map('POST', '/api/check', function(){
-	// Use URL class to check disponibility
-	$url = new com\zeroside\URL();
-	die($url->check($_POST['id']));
+$router->map('POST', '/api/check', function()
+{
+    // Use URL class to check disponibility
+    $url = new com\zeroside\URL();
+    die($url->check($_POST['id']));
 });
 
 # [API] Mapping file upload
-$router->map('POST', '/api/upload', function(){
-	function outputJSON($msg, $status = 'error'){
-    	die(json_encode(array(
-        	'data' => $msg,
-        	'status' => $status
-    	)));
-	}
-
-	// Making file informations
-	$name = $_FILES['SelectedFile']['name'];
-	$real = uniqid() . $_FILES['SelectedFile']['name'];
-	$size = human_filesize($_FILES['SelectedFile']['size']);
-	$extension = pathinfo($_FILES['SelectedFile']['name'], PATHINFO_EXTENSION);
-	$downloads = 0;
-
-	// Download URL
-	global $url;
-	$furl = $_POST['downurl'];
-	if(empty($furl)){
-		$url = uniqid();
-	} else {
-		$Checker = new com\zeroside\URL();
-		$json = json_decode($Checker->check($furl));
-
-		if($json->code == 200){
-			$url = $furl;
-		} else {
-			$url = uniqid();
-		}
-	}
-
-	// Download Expiration
-	$exp = time() + 24 * 3600;
-	if(!empty($_POST['expiration'])){
-		switch($_POST['expiration']){
-			case 1:
-				$exp = time() + 3600;
-				break;
-			case 24:
-				$exp = time() + 24 * 3600;
-				break;
-			case 48:
-				$exp = time() + 48 * 3600;
-				break;
-			case 168:
-				$exp = time() + 168 * 3600;
-				break;
-			default:
-				$exp = time + 24 * 3600;
-				break;
-		}
-	}
-
-	// Check for errors
-	if($_FILES['SelectedFile']['error'] > 0){
-    	outputJSON('An error ocurred when uploading.');
-	}
-
-	// Check for size
-	if($_FILES['SelectedFile']['size'] > 10*GB){
-		outputJSON('Too large file, 10GB max.');
-	}
-
-	// Check if the file exists
-	if(file_exists(__DIR__ . '/assets/uploads/' . $real)){
-    	outputJSON('File with that name already exists.');
-	}
-
-	// Upload file
-	if(!move_uploaded_file($_FILES['SelectedFile']['tmp_name'], __DIR__ . '/assets/uploads/' . $real)){
-    	outputJSON('Error uploading file - check destination is writeable.');
-	}
-
-	global $db;
-	global $id;
-	$request = $db->prepare("INSERT INTO `files`(`file_name`, `file_path`, `file_url`, `file_ext`, `file_size`, `file_time`, `stat_dl`, `stat_id`) VALUES (:name, :file_path, :url, :ext, :size, :file_time, :stat, :stat_id)");
-	$request->execute(array(
-		":name" => $name,
-		":file_path" => $real,
-		":url" => $url,
-		":ext" => $extension,
-		":size" => $size,
-		":file_time" => $exp,
-		":stat" => $downloads,
-		":stat_id" => $id
-	));
-
-	// Success!
-	outputJSON("File uploaded!<br><a href='https://www.zeroside.co/s/{$id}'>Check statistics</a><br><a href='https://www.zeroside.co/{$url}'>Download page</a>", 'success');
+$router->map('POST', '/api/upload', function()
+{
+    $upload = new com\zeroside\Upload;
+    $upload->upload();
 });
 
 # Match current request url
 $match = $router->match();
 
 # Sending response
-if( $match && is_callable( $match['target'] ) ) {
+if ($match && is_callable($match['target'])) {
     # Matched, send requested page
-	call_user_func_array( $match['target'], $match['params'] ); 
+    call_user_func_array($match['target'], $match['params']);
 } else {
-	# Does not match, send 404 page
-	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    # Does not match, send 404 page
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
 }
 
 ?>
